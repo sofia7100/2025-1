3d95685fad308c1b3997c6cc7b0cf27a8bea9dbf
Sofia <saalvareze@unal.edu.co>
2025-04-09 22:39:25 -0500

    codigo de hola

---
+++ codigos/hola2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+    std::string name;
+    std::cout << "hello, whats your name?\n";
+    //std::cin >>name; //lee hasta el primer espacio
+    std::getline(std:cin, name);
+    std::cout <<"hello, " + name + "\n" ;
+    return 0;
+    }
+    
