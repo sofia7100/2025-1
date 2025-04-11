25157d6b861f89c9cb1d17e1f2112af8e8547a3c
Sofia <saalvareze@unal.edu.co>
2025-04-10 21:40:25 -0500

    codigo sin error

--- codigos/hola2.cpp
+++ codigos/hola2.cpp
@@ -6,7 +6,7 @@ int main(void)
     std::string name;
     std::cout << "hello, whats your name?\n";
     //std::cin >>name; //lee hasta el primer espacio
-    std::getline(std:cin, name);
+    std::getline(std::cin, name);
     std::cout <<"hello, " + name + "\n" ;
     return 0;
     }
