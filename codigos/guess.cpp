ef3fa3a1884b5ba1ddd18fcdf2d2195ef3b26708
Sofia <saalvareze@unal.edu.co>
2025-04-11 08:40:31 -0500

    codigo con funcion

--- codigos/guess.cpp
+++ codigos/guess.cpp
@@ -9,18 +9,40 @@ Informar si lo adivino o no
 
 #include <iostream>
 
+// declaration
+void play(int success);
+
+
 int main(void) {
+    play(10);
+    play(20);
 
-    const int NUM = 10;
-    int guessed_number;
+    return 0;
+}
 
-    for( ; ; ) {
-        std::cout << "Adivina un numero entre 1 y 100:\n";
+// Implemetation
+void play(int success)
+{
+    const int NUM = success;
+    int guessed_number = NUM/2;
+
+    const int MIN = 1;
+    const int MAX = 100;
+
+    while(guessed_number != NUM) {
+        std::cout << "Adivina un numero entre  " << MIN << "  y " << MAX << ":\n";
         std::cin >> guessed_number;
-        
+        std::cout << "Ecribiste: " << guessed_number << "\n";
+
+        // validacion del rango 
+        if (guessed_number < MIN or MAX < guessed_number) {
+            std::cout << "El numero no esta en el rango.\n";
+            continue; 
+        }
+
         if (guessed_number == NUM) {
             std::cout << "GANASTE!!!!\n";
-            break;
+            //break;
         } else if (guessed_number > NUM){
             std::cout << "Te pasaste.\n";
         } else { // es menor
@@ -28,5 +50,4 @@ int main(void) {
         }
     }
 
-    return 0;
 }
